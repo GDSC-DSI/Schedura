19de4a3b9c1b305e96028eb95f21348e63545206
Janvi Kumari <janvikumarihzb39@gmail.com>
2021-12-15 17:47:02 +0530

    Home Page

--- app/src/main/java/net/code/gdsc_app/MainActivity.kt
+++ app/src/main/java/net/code/gdsc_app/MainActivity.kt
@@ -3,6 +3,9 @@ package net.code.gdsc_app
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.view.View
+import android.widget.AdapterView
+import android.widget.ArrayAdapter
+import android.widget.Spinner
 import androidx.navigation.findNavController
 import net.code.gdsc_app.databinding.ActivityMainBinding
 
@@ -15,6 +18,13 @@ class MainActivity : AppCompatActivity() {
         binding = ActivityMainBinding.inflate(layoutInflater)
         val view = binding.root
         setContentView(view)
+
+
+
+
+
+
+
 //        val navHost = findViewById<View>(R.id.navHostFragmentHome)
     }
 }
