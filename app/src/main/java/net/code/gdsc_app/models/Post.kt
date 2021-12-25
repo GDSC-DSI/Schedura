9c98b8c19a545cbf54e4b8f3504e5c1159197328
pareekshit28 <pareekshitjoshi28@gmail.com>
2021-12-26 01:26:10 +0530

    Commit before merge

---
+++ app/src/main/java/net/code/gdsc_app/models/Post.kt
@@ -0,0 +1,8 @@
+package net.code.gdsc_app.models
+
+data class Post(
+    val userId : Int,
+    val id : Int,
+    val title : String,
+    val body : String
+)
--- app/src/main/java/net/code/gdsc_app/models/User.kt
+++
@@ -1,9 +0,0 @@
-package net.code.gdsc_app.models
-
-data class User(
-    val postId : Int,
-    val id : Int,
-    val name : String,
-    val email : String,
-    val body : String
-)
