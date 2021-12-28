b362d323c548c3f193c8b8e9530fe02bc87582e3
pareekshit28 <pareekshitjoshi28@gmail.com>
2021-12-28 22:16:18 +0530

    Implemented Attendance

--- app/src/main/java/net/code/gdsc_app/models/Post.kt
+++ app/src/main/java/net/code/gdsc_app/models/Post.kt
@@ -1,8 +1,9 @@
 package net.code.gdsc_app.models
 
 data class Post(
-    val userId : Int,
+    val postId : Int,
     val id : Int,
-    val title : String,
+    val name : String,
+    val email : String,
     val body : String
 )
